3eff47cc2207dd108465cb0da2f9c5a3b205dbd3
Stefan <stefke2105996@gmail.com>
2017-04-29 21:00:02 +0200

    "fixed" UsageRights, completed register & sign-in, modified nav menu

--- app/src/main/java/rs/stefandjokic/mokranjatzz365bet/Register.java
+++ app/src/main/java/rs/stefandjokic/mokranjatzz365bet/Register.java
@@ -1,7 +1,10 @@
 package rs.stefandjokic.mokranjatzz365bet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+    }
+
+    public void usageRights(View view) {
+
+        startActivity(new Intent(this, UsageRights.class));
+
     }
 }
