2de36a7ebcac076afe1079c07e3e9d6f64302937
Stefan <stefke2105996@gmail.com>
2017-04-30 10:45:42 +0200

    adapted usage rights txt

--- app/src/main/java/rs/stefandjokic/mokranjatzz365bet/UsageRights.java
+++ app/src/main/java/rs/stefandjokic/mokranjatzz365bet/UsageRights.java
@@ -2,8 +2,6 @@ package rs.stefandjokic.mokranjatzz365bet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
 
 public class UsageRights extends AppCompatActivity {
 
@@ -11,10 +9,6 @@ public class UsageRights extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usage_rights);
-
-        TextView textView = (TextView) (findViewById(R.id.usage_rights_txt));
-        textView.setMovementMethod(new ScrollingMovementMethod());
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
