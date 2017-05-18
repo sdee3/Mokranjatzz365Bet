972a48ca2d113fee7b895e48e22fb2c2e71dc12c
Stefan <stefke2105996@gmail.com>
2017-05-18 17:25:44 +0200

    changed ip

--- app/src/main/java/rs/stefandjokic/mokranjatzz365bet/activities/MainActivity.java
+++ app/src/main/java/rs/stefandjokic/mokranjatzz365bet/activities/MainActivity.java
@@ -26,16 +26,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Inicijalizacija banner-a
-        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+        initializeBanner();
+        showBanner();
 
-        //Banner-e, pojavi se!
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        //Ako je User već ulogovan:
-        if(SharedPreferencesManager.getInstance(this.getApplicationContext()).isLoggedIn()){
+        if(userLoggedIn()){
             finish();
             startActivity(new Intent(this, HomeActivity.class));
         }
@@ -43,40 +37,44 @@ public class MainActivity extends AppCompatActivity {
         this.registerReceiver(this.finishAlert, new IntentFilter(FINISH_ALERT));
     }
 
+    private boolean userLoggedIn() {
+        return SharedPreferencesManager.getInstance(this.getApplicationContext()).isLoggedIn();
+    }
+
+    private void initializeBanner() {
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+    }
+
+    private void showBanner() {
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
     public void loginActivity(View view) {
-
         startActivity(new Intent(this, SignInActivity.class));
-
     }
 
     public void registerActivity(View view) {
-
         startActivity(new Intent(this, RegisterActivity.class));
-
     }
 
     public void launchMokranjacInfo(View view) {
-        //Open Wiki page: https://sr.wikipedia.org/sr-el/%D0%A1%D1%82%D0%B5%D0%B2%D0%B0%D0%BD_%D0%A1%D1%82%D0%BE%D1%98%D0%B0%D0%BD%D0%BE%D0%B2%D0%B8%D1%9B_%D0%9C%D0%BE%D0%BA%D1%80%D0%B0%D1%9A%D0%B0%D1%86
-
         String url = "https://sr.wikipedia.org/sr-el/%D0%A1%D1%82%D0%B5%D0%B2%D0%B0%D0%BD_%D0%A1%D1%82%D0%BE%D1%98%D0%B0%D0%BD%D0%BE%D0%B2%D0%B8%D1%9B_%D0%9C%D0%BE%D0%BA%D1%80%D0%B0%D1%9A%D0%B0%D1%86";
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
-
     }
 
     BroadcastReceiver finishAlert = new BroadcastReceiver() {
-
         @Override
         public void onReceive(Context context, Intent intent) {
-
             MainActivity.this.finish();
         }
     };
 
     @Override
     public void onDestroy() {
-
         super.onDestroy();
         this.unregisterReceiver(finishAlert);
     }
--- app/src/main/java/rs/stefandjokic/mokranjatzz365bet/api/API_URL.java
+++ app/src/main/java/rs/stefandjokic/mokranjatzz365bet/api/API_URL.java
@@ -2,6 +2,6 @@ package rs.stefandjokic.mokranjatzz365bet.api;
 
 public class API_URL {
 
-    public static final String BASE_URL = "http://192.168.1.2:8012/Retrofit/public/";
+    public static final String BASE_URL = "http://93.86.138.24/php/Mokranjatzz/public/";
 
 }
